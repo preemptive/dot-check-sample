9b753afc24499fdcf2e5a55608d00cd28db3e73e
leach <bkl@preemptive.com>
2017-04-04 12:10:00 -0400

    Add third party licensing

--- DotCheckSample.Wpf/ApplicationInsightsHelper.cs
+++ DotCheckSample.Wpf/ApplicationInsightsHelper.cs
@@ -1,4 +1,16 @@
-﻿// This is a modified version of code from here: https://github.com/DeadlyEmbrace/ApplicationInsightExample
+﻿// Copyright (c) 2017 PreEmptive Solutions; All Right Reserved, http://www.preemptive.com/
+//
+// This source is subject to the Microsoft Public License (MS-PL).
+// Please see the License.txt file for more information.
+// All other rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+
+// This is a modified version of MIT Licensed code, initially from here: https://github.com/DeadlyEmbrace/ApplicationInsightExample
+// See ThirdPartyLicenses.txt
 
 using System;
 using System.Collections.Generic;
